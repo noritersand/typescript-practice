27dc3afb0544bf9cd048bbb432ef0829667c6604
fixalot <fixalot@anyssign.com>
2023-12-05 15:15:45 +0900

    Update

--- index.ts
+++ index.ts
@@ -1,5 +1,12 @@
-import {PI} from './src/constant.js';
+import {PI} from './src/constant';
 
 console.log("Hello via Bun!");
 console.log('PI:', PI);
+
+function fn(n: number) {
+  console.log('n:', n);
+}
+
+// fn(null); // error TS2345: Argument of type 'null' is not assignable to parameter of type 'number'.
+// let foo: number = null; // error TS2322: Type 'null' is not assignable to type 'number'.
 
