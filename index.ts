1772d60d9d8f1442a50fd13ed7df8f91e19891bd
fixalot <fixalot@anyssign.com>
2023-12-10 13:04:53 +0900

    Update

--- index.ts
+++ index.ts
@@ -9,4 +9,3 @@ function fn(n: number) {
 
 // fn(null); // error TS2345: Argument of type 'null' is not assignable to parameter of type 'number'.
 // let foo: number = null; // error TS2322: Type 'null' is not assignable to type 'number'.
-
