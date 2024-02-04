0188167b67e3769d45baf54544ecfd89464037a9
fixalot <fixalot@anyssign.com>
2024-02-05 00:41:02 +0900

    Update

--- src/practice/types.ts
+++ src/practice/types.ts
@@ -1,4 +1,7 @@
-function printFooBar2(foobar: { foo: string, bar: number }) {
-  console.log(foobar.foo, foobar.bar);
-}
-printFooBar2({ foo: 'foo', bar: 1 });
+let getWaldo = (name: string): {
+  name: string,
+  age?: number
+} => {
+  return {name}
+};
+getWaldo('Waldo').age = 128;
