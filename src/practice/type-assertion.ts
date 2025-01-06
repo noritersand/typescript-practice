e6d0a074e9d5e1f9ce308570921a2ea492f55a48
fix-a-lot <fixalot@outlook.kr>
2025-01-06 11:32:56 +0900

    Update

---
+++ src/practice/type-assertion.ts
@@ -0,0 +1,8 @@
+let value: unknown = "Hello, TypeScript!";
+
+// console.log((value).toUpperCase());
+// error TS2571: Object is of type 'unknown'.
+// value는 unknown이기 때문에 바로 toUpperCase() 호출 불가
+
+console.log((value as string).toUpperCase());
+
